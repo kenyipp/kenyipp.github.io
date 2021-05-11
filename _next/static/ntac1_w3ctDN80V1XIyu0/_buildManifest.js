4cb173b5c7499accf8a5e7d5b65e1f42f96d4ca8
Ken Yip <ken20206@gmail.com>
2021-05-11 21:55:56 +0800

    Deploy Next.Js application to github page

---
+++ _next/static/ntac1_w3ctDN80V1XIyu0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-726b1245a5893b57b488.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fffd8d6d3d8c3c70b74c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
